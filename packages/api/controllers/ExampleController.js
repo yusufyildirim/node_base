86b779a370999722e85aa2fd21c3ef70a19a5e66
Yusuf YILDIRIM <lostofis@gmail.com>
2018-12-10 12:34:55 +0300

    Example controller improvements.

--- packages/api/controllers/ExampleController.js
+++ packages/api/controllers/ExampleController.js
@@ -1,13 +1,15 @@
 import { LogicError } from '../utils/errors';
 
 async function root(req, res, next) {
+  // const {} = req.body;
+
   try {
     // throw new LogicError('test_error')
     return res.json({ result: 'ok' });
   } catch (e) {
     // if you throw an error in the try block, it'll catch in this catch block
     // and it'll continue to the our error handling middleware
-    next(e);
+    return next(e);
   }
 }
 
